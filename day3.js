26cb1e7b77d9e2b2ddd31c4bb28562ae0e6eb767
Joey Cozza <joeycozza@gmail.com>
2021-12-06 15:13:46 -0700

    day3 part 2 done

--- day3.js
+++ day3.js
@@ -1,31 +1,61 @@
 const _ = require('lodash')
 const fs = require('fs')
 
-const lines = fs.readFileSync('./input/day3b.txt', 'utf8').split('\n').filter(Boolean)
+const lines = fs.readFileSync('./input/day3.txt', 'utf8').split('\n').filter(Boolean)
 
 const bitLength = lines[0].length
 const inputLength = lines.length
-const gamma = []
-const epsilon = []
+let oxyGenRating
+let c02Rating
 
-for (let i = 0; i < bitLength; i++) {
-  const oneCount = lines.reduce((sum, line) => {
-    if (line[i] === '1') {
+let currentLines = _.cloneDeep(lines)
+
+let i = 0
+while (currentLines.length > 1) {
+  let commonBit = mostCommonBit(currentLines, i)
+  if (commonBit === '-1') {
+    commonBit = '1'
+  }
+  currentLines = currentLines.filter((line) => {
+    return line[i] === commonBit
+  })
+  i++
+}
+oxyGenRating = currentLines[0]
+
+currentLines = _.cloneDeep(lines)
+i = 0
+while (currentLines.length > 1) {
+  let commonBit = leastCommonBit(currentLines, i)
+  if (commonBit === '-1') {
+    commonBit = '0'
+  }
+  currentLines = currentLines.filter((line) => {
+    return line[i] === commonBit
+  })
+  i++
+}
+c02Rating = currentLines[0]
+
+console.log('oxyGenRating: ', oxyGenRating)
+console.log('c02Rating: ', c02Rating)
+console.log('countBinary(oxyGenRating): ', countBinary(oxyGenRating))
+console.log('countBinary(c02Rating): ', countBinary(c02Rating))
+
+function leastCommonBit(array, index) {
+  const commonBit = mostCommonBit(array, index)
+  return commonBit === '1' ? '0' : commonBit === '0' ? '1' : '-1'
+}
+
+function mostCommonBit(array, index) {
+  const oneCount = array.reduce((sum, line) => {
+    if (line[index] === '1') {
       sum += 1
     }
     return sum
   }, 0)
-  const zeroCount = inputLength - oneCount
-
-  if (oneCount > zeroCount) {
-    mostCommon = '1'
-    leastCommon = '0'
-  } else {
-    mostCommon = '0'
-    leastCommon = '1'
-  }
-  gamma.push(mostCommon)
-  epsilon.push(leastCommon)
+  const zeroCount = array.length - oneCount
+  return oneCount > zeroCount ? '1' : zeroCount > oneCount ? '0' : '-1'
 }
 
 function countBinary(bits) {
@@ -42,9 +72,3 @@ function countBinary(bits) {
     0
   )
 }
-
-console.log('gamma: ', gamma)
-console.log('countBinary(gamma): ', countBinary(gamma))
-console.log('epsilon: ', epsilon)
-console.log('countBinary(epsilon): ', countBinary(epsilon))
-console.log(' countBinary(gamma) * countBinary(epsilon): ', countBinary(gamma) * countBinary(epsilon))
